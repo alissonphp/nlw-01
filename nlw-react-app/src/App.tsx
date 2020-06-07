dc4482462e4a7c6ed9bac1b1d400a956a7bd0038
Alisson Gomes <alisson.php@gmail.com>
2020-06-07 13:01:31 -0300

    up(project): initial commit

---
+++ nlw-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Routes from './routes'
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Routes />
+    </React.StrictMode>
+  );
+}
+
+export default App;
